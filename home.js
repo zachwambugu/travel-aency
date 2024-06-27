8175e26ea0efa13e65e29ca6e82f9ae566a51c0e
zachwambugu <118126753+zachwambugu@users.noreply.github.com>
2024-06-28 02:38:09 +0300

    Delete home.js

--- home.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('scroll', function() {
-    const footer = document.getElementById('footer');
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-        footer.style.display = 'block';
-    } else {
-        footer.style.display = 'none';
-    }
-});
